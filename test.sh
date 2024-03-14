5d76c5bac6062fe295737efa9bfbb98d42accac6
rmens <r.mens@me.com>
2024-03-14 22:25:42 +0100

    Uploaden

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-# FFmpeg installeren
-apt update -y; apt install ffmpeg -y
+# StereoTool installeren
+wget https://www.stereotool.com/download/stereo_tool_cmd_64 -O /opt/stereotool
+chmod +x /opt/stereotool
 
-# StereoTool downloaden
-wget https://www.stereotool.com/download/stereo_tool_cmd_64 -O /usr/bin/stereotool
-chmod +x /usr/bin/stereotool
+# FFMmpeg installeren
+apt update -y; apt install ffmpeg -y
 
 # Oude noodband verwijderen
 rm /tmp/noodband.flac
@@ -14,8 +14,8 @@ else
     echo "Bestand /tmp/noodband.mp3 bestaat al. Data is duur op Azure, dus downloaden het niet opnieuw!."
 fi
 
-# Boel pipen (verwijder -t 60 voor hele uur)
+# Boel pipen (verwijder -t 600 voor hele uur, anders 10min)
 ffmpeg -hide_banner -i /tmp/noodband.mp3 -t 600 -f wav -acodec pcm_s16le -ar 192000 -ac 2 - | stereotool -q -r 192000 - - -s /root/st.ini | ffmpeg -hide_banner -i - -f wav -ac 1 -ar 192000 /tmp/noodband.flac
 
-
-
+# Gegenereerd bestand uploaden
+curl --silent bashupload.com -T /tmp/noodband.flac
